34f89ee0095905f0d357e711c126cc7e6adf4623
VonHyde <dariojze@gmail.com>
2025-05-12 22:10:08 +0200

    Added EDA and ML

---
+++ src/ab_protocol_map.py
@@ -0,0 +1,25 @@
+AB_PROTOCOLS_GENERAL = {
+    "first_line": [
+        "penicillin", "benzylpenicillin", "ampicillin", "amoxicillin", "amoxicillin-clavulanic_acid",
+        "piperacillin-tazobactam", "cephalexin", "cefalexin", "cefazolin", "cephalothin",
+        "cefalotin", "cefuroxime", "cefamandole", "cefoxitin", "trimethoprim-sulfamethoxazole",
+        "sulfamethoxazole", "sulfisoxazole", "sulfadimethoxine", "azithromycin", "erythromycin",
+        "clarithromycin", "gentamicin", "tobramycin", "spectinomycin"
+    ],
+
+    "second_line": [
+        "cefotaxime", "ceftriaxone", "ceftazidime", "ceftazidime-avibactam", "cefepime",
+        "ceftaroline", "cefiderocol", "ciprofloxacin", "levofloxacin", "moxifloxacin",
+        "ofloxacin", "norfloxacin", "doxycycline", "minocycline", "tetracycline", "chloramphenicol",
+        "rifampin", "fosfomycin", "clindamycin", "linezolid", "trimethoprim", "colistin"
+    ],
+
+    "reserve": [
+        "meropenem", "imipenem", "ertapenem", "doripenem", "polymyxin_b", "tigecycline",
+        "vancomycin", "teicoplanin", "oritavancin", "daptomycin", "synercid",
+        "quinupristin-dalfopristin", "tedizolid", "isavuconazole", "amphotericin_b",
+        "ceftolozane-tazobactam", "ceftazidime-clavulanic_acid", "cefotaxime-clavulanic_acid",
+        "fluconazole", "posaconazole", "voriconazole", "rifabutin", "ethionamide",
+        "cycloserine", "pyrazinamide", "capreomycin", "plazomicin", "mupirocin", "fusidic_acid"
+    ]
+}
